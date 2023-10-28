725027fe7bfce5e2191a1137a7c17e7d4d7e83f1
lukewire <lukewire129@gmail.com>
2023-10-28 11:55:09 +0900

    Updates .

---
+++ src/DragAnimatedBox.Example2/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using DragAnimatedBox.Example.ViewModels;
+using System.Windows;
+
+namespace DragAnimatedBox.Example2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent ();
+            this.DataContext = new MainViewModel ();
+        }
+    }
+}
---
+++ src/DragAnimatedBox.Example2/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DragAnimatedBox.Event;
+using System.Collections.ObjectModel;
+
+namespace DragAnimatedBox.Example.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty] ObservableCollection<string> testData;
+
+        public MainViewModel()
+        {
+            this.TestData = new ();
+            TestData.Add ("Item1");
+            TestData.Add ("Item2");
+            TestData.Add ("Item3");
+            TestData.Add ("Item4");
+            TestData.Add ("Item5");
+        }
+
+        [RelayCommand]
+        private void Swap(DragDropArgs<int, int> obj)
+        {
+            testData.Move (obj.TargetItem, obj.DropItem);
+        }
+    }
+}
